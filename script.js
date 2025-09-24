68e12939977a9ea1a8fe4d2002e6cce79715830f
Bóka Tamás <tomi500offical@gmail.com>
2025-09-24 09:46:30 +0200

    object array and table generation done

---
+++ script.js
@@ -0,0 +1,34 @@
+const values = Array.from({length: 20}, (_, index) => {
+    const value = Math.floor(Math.random()*10000)+1;
+    return {
+        id: index + 1,
+        value: value,
+        type: value % 2 === 0 ? 'páros' : 'páratlan',
+        category: value < 3000 ? "szegények pénze" : (value >= 3000 && value <= 6000) ? "középosztály pénze" : "gazdagok pénze"
+    }
+})
+
+
+function createTableRow(obj, parent) {
+    const tr = document.createElement("tr")
+    for (const key of Object.keys(obj)) {
+        const td = document.createElement("td")
+        td.innerHTML = obj[key];
+        tr.appendChild(td)
+    }
+    parent.appendChild(tr)
+}
+
+function createTable(arr, div) {
+    const table = document.createElement("table")
+    table.classList.add("table")
+    table.id = "table";
+    div.appendChild(table)
+    for (const obj of arr) {
+        createTableRow(obj, table)
+    }
+}
+
+const tableDiv = document.getElementById("tableDiv")
+
+createTable(values, tableDiv)
